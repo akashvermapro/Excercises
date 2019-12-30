d845fa7be7493f3c50a67a3a2cba54d69dd52326
Akash Verma <47306321+akashvermapro@users.noreply.github.com>
2019-12-30 15:21:00 +0530

    Delete CompareById.java

--- com/Employee/CompareById.java
+++
@@ -1,9 +0,0 @@
-package com.deloitte;
-import java.util.Comparator;
-
-public class CompareById implements Comparator<Employee>{
-	public int compare(Employee e1,Employee e2){
-		return e1.getEmpId()-e2.getEmpId();
-	}
-
-}
