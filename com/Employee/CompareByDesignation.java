b7dfca94a51cd332a1d46052ec0f30d91b3f2174
Akash Verma <47306321+akashvermapro@users.noreply.github.com>
2019-12-30 15:20:44 +0530

    Delete CompareByDesignation.java

--- com/Employee/CompareByDesignation.java
+++
@@ -1,8 +0,0 @@
-package com.deloitte;
-import java.util.Comparator;
-public class CompareByDesignation implements Comparator<Employee> {
-	public int compare(Employee e1,Employee e2){
-		return e1.getDesignation().compareTo(e2.getDesignation());
-	}
-
-}
